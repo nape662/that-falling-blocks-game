44813aceb0a3d5fb24fba12ec9ff31f4950ce445
Nape662 <peter.samodelkin@gmail.com>
2023-06-29 12:17:55 +0200

    does this fix red stuff in pycharm?

---
+++ app.py
@@ -0,0 +1,15 @@
+
+import constants
+import pygame as pg
+
+class App:
+    def __init__(self, width, height):
+        pg.init()
+        pg.display.set_caption("Dots")
+        self.screen = pg.display.set_mode([WINDOW_WIDTH, WINDOW_HEIGHT])
+
+        self.width = width
+        self.height = height
+        self.grid = self.grid = [[0 for _ in range(width)] for _ in range(height)]
+        self.score = 0
+
---
+++ testing_functions.py
@@ -0,0 +1,7 @@
+
+a = [[0 for _ in range(3)] for _ in range(4)]
+b = [i for i in range(3)]
+c = [1, 2, 3]
+print(a[1])
+print(b)
+print(c[1])
