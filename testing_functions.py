14aaf16133f2aac0c3b21a786b21397a1e015fde
Nape662 <peter.samodelkin@gmail.com>
2023-07-01 08:22:41 +0200

    please ignore this

--- testing_functions.py
+++ testing_functions.py
@@ -5,3 +5,18 @@ c = [1, 2, 3]
 print(a[1])
 print(b)
 print(c[1])
+
+def rooty():
+    res = 0
+    for i in range(10, 101):
+        k = i
+        print("{} = i".format(i))
+        for j in range(i-9, i):
+            print(j, end = " ")
+            k *= j/(100-(i-j))
+        print(k)
+        res += k
+    print(res)
+
+rooty()
+
